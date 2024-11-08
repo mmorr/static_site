da708f458ae49c9440d9b422951c487737637369
Mark Morrison <mark@morrison.io>
2024-11-07 17:39:35 -0800

    add LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class testHTMLNode(unittest.TestCase):
     def test_print(self):
@@ -13,4 +13,8 @@ class testHTMLNode(unittest.TestCase):
     def test_toprops2(self):
         node = HTMLNode("p", "This is a HTML paragraph", None, {"class": "paragraph"})
         self.assertEqual(node.props_to_html(), 'class="paragraph"')
+
+    def test_tohtml(self):
+        node = LeafNode("h1", "This is an HTML header", {"class": "header"})
+        self.assertEqual(node.to_html(), '<h1 class="header">This is an HTML header</h1>')
 
